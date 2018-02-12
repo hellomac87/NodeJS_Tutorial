43d52a1256799e33e1316fcbafd8745679fee969
leegangsan <nouvin@certon.co.kr>
2018-02-12 14:59:20 +0900

    session-01

---
+++ app_session.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var session = require('express-session');
+var app = express();
+
+app.use(session({
+  secret: 'somerandomvalue',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.get('/count', function(req, res){
+  if(req.session.count){
+    req.session.count++;
+  }else{
+    req.session.count = 1;
+  }
+  res.send('count : ' + req.session.count);
+});
+
+app.listen(3000, function(){
+    console.log('Connected 3000 port');
+});
