884cbef5af2bb06c03948e43b3ed8d17a2ca6cb1
leegangsan <nouvin@certon.co.kr>
2018-02-12 18:16:26 +0900

    Session 5 : login 2

--- app_session.js
+++ app_session.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var session = require('express-session');
+var bodyParser = require('body-parser')
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(session({
   secret: 'somerandomvalue',
   resave: false,
@@ -16,6 +18,40 @@ app.get('/count', function(req, res){
   }
   res.send('count : ' + req.session.count);
 });
+
+app.post('/auth/login', function(req, res){
+  var user = {
+    username : 'egoing',
+    password : '111'
+  };
+
+  var uname = req.body.username;
+  var pwd = req.body.password;
+
+  if(uname === user.username && pwd === user.password){
+    res.redirect('/welcome');
+  }else{
+    res.send('there is no user <a href="/auth/login">login</a>');
+  }
+
+});
+app.get('/auth/login', function(req, res){
+  var output = `
+    <h1>Login</h1>
+    <form action='/auth/login' method='post'>
+    <p>
+    <input type='text' name='username' placeholder='username'>
+    </p>
+    <p>
+    <input type='password' name='password' placeholder='password'>
+    </p>
+    <p>
+    <input type='submit'>
+    </p>
+    </form>
+  `;
+  res.send(output);
+})
 
 app.listen(3000, function(){
     console.log('Connected 3000 port');
