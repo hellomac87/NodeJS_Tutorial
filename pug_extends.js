3ac426180410e65a1ae20c11a86f2a96e5a15d7b
leegangsan <nouvin@certon.co.kr>
2018-02-27 10:44:02 +0900

    jade - extends

---
+++ pug_extends.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', 'pug');
+app.get('/view', function(req, res){
+  res.render('view');
+});
+app.get('/add', function(req, res){
+  res.render('add');
+});
+
+app.listen(3000, function(){
+  console.log('connect 3000 port!');
+})
